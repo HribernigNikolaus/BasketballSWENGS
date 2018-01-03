d4cce982ea07abfb5d556dd3f94ea13168b279e5
Vanessa Fortmüller <vanessa.fortmueller@edu.fh-joanneum.at>
2018-01-03 17:29:52 +0100

    Stadium Card ts and html

--- basketball-app/src/basketball/stadium/stadium-card/stadium-card.component.ts
+++ basketball-app/src/basketball/stadium/stadium-card/stadium-card.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Stadium} from "../../entities/stadium";
 
 @Component({
   selector: 'stadium-card',
-  templateUrl: './stadium-card.component.html',
-  styleUrls: ['./stadium-card.component.css']
+  templateUrl: './stadium-card.component.html'
 })
 export class StadiumCardComponent implements OnInit {
 
   constructor() { }
+  @Input() item: Stadium;
+  @Input() selected: boolean;
+  @Output() selectedChange = new EventEmitter<boolean>();
 
   ngOnInit() {
   }
+  select() {
+    this.selected = true;
+    this.selectedChange.next(this.selected);
+  }
+  deselect() {
+    this.selected = false;
+    this.selectedChange.next(this.selected);
+  }
+
+
 
 }
